290e0be30f52fdc28b0773231163c535348bfce8
camperik94 <tomaszjach94@gmail.com>
2017-12-24 20:50:35 +0100

    bmi

--- src/Bmi.java
+++ src/Bmi.java
@@ -1,4 +1,33 @@
 
 public class Bmi {
+	private int wzrost;
+	private int waga;
+
+	void setWzrost(int wzrost) {
+		this.wzrost = wzrost;
+	}
+
+	int getWzrost() {
+		return wzrost;
+	}
+
+	void setWaga(int waga) {
+		this.waga = waga;
+	}
+
+	int getWaga() {
+		return waga;
+	}
 	
+	Bmi(int waga, int wzrost){
+		this.waga = waga;
+		this.wzrost = wzrost;
+	}
+	
+	void zmierzBmi(){
+		
+		double bmi = waga / (wzrost * wzrost);
+		System.out.println(bmi);
+		
+	}
 }
--- src/Zadanie.java
+++ src/Zadanie.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
 
 public class Zadanie {
 	public static void main(String[] args) {
+		Scanner in = new Scanner(System.in);
+		int weight = in.nextInt();
+		int height = in.nextInt();
+		
+		
+		in.close();
+		
+		Bmi bmi = new Bmi(weight, height);
+		
+		
+		System.out.println(bmi.getWaga());
+		System.out.println(bmi.getWzrost());
+		bmi.zmierzBmi();
+		
 		
 	}
 }
